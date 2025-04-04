08798431dea87fd950f4c42733e384c2770ee1e4
ELRabito <39779934+ELRabito@users.noreply.github.com>
2025-04-04 13:38:18 +0200

    Update ExilePlayerCargoSaving_SQL_Fixes.sql

--- ExilePlayerCargoSaving_SQL_Fixes.sql
+++ ExilePlayerCargoSaving_SQL_Fixes.sql
@@ -5,3 +5,20 @@ SET backpack = JSON_ARRAY(
     JSON_ARRAY()
 )
 WHERE backpack IS NOT NULL;
+
+
+-- If the tope one doesn't work use this query. 
+-- Fix backpack column (MySQL 5.7 version)
+UPDATE player
+SET backpack = CONCAT(
+    '["',
+    TRIM(BOTH '"' FROM backpack),
+    '", []]'
+)
+WHERE backpack IS NOT NULL;-- Fix backpack column
+UPDATE player
+SET backpack = JSON_ARRAY(
+    TRIM(BOTH '"' FROM backpack),
+    JSON_ARRAY()
+)
+WHERE backpack IS NOT NULL;
